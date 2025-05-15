187a05b5de2e05a9b05fd99b5496a8ea0cde0ddc
Yuji Yamaguchi <yuji.ym@gmail.com>
2025-05-15 14:48:29 +0900

    Revert "use hono"
    This reverts commit f17833b3318d1f05f26f3e3135f01dd141fc724e.

--- packages/frontend/workers/app.ts
+++ packages/frontend/workers/app.ts
@@ -1,7 +1,6 @@
 import { type DrizzleD1Database, drizzle } from "drizzle-orm/d1";
 import { createRequestHandler } from "react-router";
-import * as schema from "db/schema";
-import { getApp } from "server";
+import * as schema from "../db/schema";
 
 declare module "react-router" {
   export interface AppLoadContext {
@@ -18,23 +17,12 @@ const requestHandler = createRequestHandler(
   import.meta.env.MODE,
 );
 
-// -- use hono
-const app = getApp(async (request, env, ctx) => {
-  return requestHandler(request, {
-    cloudflare: { env, ctx },
-    db: drizzle(env.DB, { schema }),
-  });
-});
-
-export default app satisfies ExportedHandler<Env>;
-
-// -- default
-// export default {
-//   async fetch(request, env, ctx) {
-//     const db = drizzle(env.DB, { schema });
-//     return requestHandler(request, {
-//       cloudflare: { env, ctx },
-//       db,
-//     });
-//   },
-// } satisfies ExportedHandler<Env>;
+export default {
+  async fetch(request, env, ctx) {
+    const db = drizzle(env.DB, { schema });
+    return requestHandler(request, {
+      cloudflare: { env, ctx },
+      db,
+    });
+  },
+} satisfies ExportedHandler<Env>;
